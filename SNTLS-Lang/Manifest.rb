b29c38c67f4b27e0e29ed0a7af285671be03201b
Jonas <jonaskorte2904@gmail.com>
2021-01-19 08:10:34 +0100

    Add Hello, World Example Project

--- SNTLS-Lang/Manifest.rb
+++ SNTLS-Lang/Manifest.rb
@@ -1,9 +1,10 @@
 class Manifest
-    def initialize(sources, modules, source_path, output_path, output_type, output_platform, icon_path, resource_path, application_version, compiler_version)
+    def initialize(sources, modules, source_path, output_path, intermediate_directory, output_type, output_platform, icon_path, resource_path, application_version, compiler_version)
         @sources = sources
         @modules = modules
         @source_path = source_path
         @output_path = output_path
+        @intermediate_directory = intermediate_directory
         @output_type = output_type
         @output_platform = output_platform
         @icon_path = icon_path
@@ -11,5 +12,4 @@ class Manifest
         @application_version = application_version
         @compiler_version = compiler_version
     end
-
 end

bac7037df5dff8c4399d66110d49e9d50e5c2116
Jonas <jonaskorte2904@gmail.com>
2021-01-21 09:25:26 +0100

    Update ManifestParser

--- SNTLS-Lang/ManifestParser.rb
+++ SNTLS-Lang/ManifestParser.rb
@@ -2,4 +2,16 @@ require_relative 'Manifest'
 
 def parseManifest(data)
     manifest = Manifest.new()
+
+    variable = ""
+    tok = ""
+    for ch in source.split(//)
+        tok += c
+        if variable == "sources":
+        end
+        if tok == "sources"
+            variable = tok
+            tok = ""
+        end
+    end
 end
--- SNTLS-Lang/SourceSplitter.rb
+++ SNTLS-Lang/SourceSplitter.rb
@@ -4,51 +4,49 @@ def split(source)
     tokens = []
     num_tokens = {}
     ident = false
-    for line in source.split("\n")
         tok = ""
-        for ch in line.split(//)
+        for ch in source.split(//)
             if ident
                 ident = false
                 if ch == " "
                     if !num_tokens[tok]
                         num_tokens[tok] = 0
                     end
-                    token = Token.new(num_tokens[tok], "identifier", tok)
-                    tokens += token
-                    num_tokens[tok] += 1
-                end
-            end
-
-            tok += ch
-            if ch == " "
-                if !num_tokens["space"]
-                    num_tokens["space"] = 0
-                end
-                token = Token.new(num_tokens["space"], "space", "")
+                token = Token.new(num_tokens[tok], "identifier", tok)
                 tokens += token
-                num_tokens["space"] += 1
+                num_tokens[tok] += 1
+            end
+        end
+
+        tok += ch
+        if ch == " "
+            if !num_tokens["space"]
+                num_tokens["space"] = 0
+            end
+            token = Token.new(num_tokens["space"], "space", "")
+            tokens += token
+            num_tokens["space"] += 1
                 
 
-                tok = ""
+            tok = ""
+        end
+        if ch == "\n"
+            if !num_tokens["newline"]
+                num_tokens["newline"] = 0
             end
-            if ch == "\n"
-                if !num_tokens["newline"]
-                    num_tokens["newline"] = 0
-                end
-                token = Token.new(num_tokens["newline"], "newline", "")
-                tokens += token
-                num_tokens["newline"] += 1
-                tok = ""
+            token = Token.new(num_tokens["newline"], "newline", "")
+            tokens += token
+            num_tokens["newline"] += 1
+            tok = ""
+        end
+        if ch == "@"
+            if !num_tokens["at"]
+                num_tokens["at"] = 0
             end
-            if ch == "@"
-                if !num_tokens["at"]
-                    num_tokens["at"] = 0
-                end
-                token = Token.new(num_tokens["at"], "operator", "@")
-                tokens += token
-                num_tokens["at"] += 1
-                tok = ""
-            end
+            token = Token.new(num_tokens["at"], "operator", "@")
+            tokens += token
+            num_tokens["at"] += 1
+            tok = ""
         end
     end
     return tokens
